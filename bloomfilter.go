1cac567bebbd9c1358fadafafa8f34bbc4867777
Alberto Bregliano <alberto.bregliano@pm.me>
2019-12-28 02:34:59 +0100

    ok

--- bloomfilter.go
+++ bloomfilter.go
@@ -1,8 +1,9 @@
 package bloomfilter
 
 import (
-	"bytes"
+	"fmt"
 	"math/bits"
+	"strconv"
 	"sync"
 
 	"github.com/axamon/hashstring"
@@ -10,7 +11,7 @@ import (
 
 // BloomFilter ...
 type BloomFilter struct {
-	Buf *bytes.Buffer
+	Slice uint
 	sync.Mutex
 }
 
@@ -18,8 +19,7 @@ type BloomFilter struct {
 func New() *BloomFilter {
 
 	var filter = new(BloomFilter)
-
-	filter.Buf = bytes.NewBuffer(make([]byte, 5000000))
+	filter.Slice = uint(1000000)
 
 	return filter
 }
@@ -27,7 +27,41 @@ func New() *BloomFilter {
 // HashToInt ...
 func HashToInt(s string) int {
 
-	hashstring.Md5Sum(s)
+	f := New()
+
+	
+	h := hashstring.Sha256Sum(s)
+
+	ss := fmt.Sprintf("%x\n", h)
+
+	// var k int
+	// for _, v := range ss {
+	// 	k = k + int(v)
+	// }
+
+	n, _ := strconv.Atoi(ss)
+
+	f.Slice |= (1 << uint(n))
+
+	fmt.Println(f.Slice)
+
+
+	fmt.Println()
+	// fmt.Println(h, n)
+	//fmt.Println(l, int(v), k)
+
+	// value := uint(1000000)
+	// // Loop over all bits in the uint.
+	for i := 0; i < bits.UintSize; i++ {
+		// If there is a bit set at this position, write a 1.
+		// ... Otherwise write a 0.
+		if f.Slice&(1<<uint(i)) != 0 {
+			fmt.Print("1")
+		} else {
+			fmt.Print("0")
+		}
+		//value |= (1 << uint(30))
+	}
 
 	return 0
 }
@@ -41,6 +75,7 @@ func CountOneBits(i uint) int {
 func (f *BloomFilter) SetBit(nbit int) {
 	f.Lock()
 	defer f.Unlock()
-	1
+
+	return
 
 }
--- bloomfilter_test.go
+++ bloomfilter_test.go
@@ -1,24 +1,14 @@
 package bloomfilter_test
 
 import (
-	"reflect"
-	"testing"
 	"github.com/axamon/bloomfilter"
 )
 
-func TestNew(t *testing.T) {
-	tests := []struct {
-		name string
-		want *BloomFilter
-	}{
-		// TODO: Add test cases.
-		{name: "first", want: }
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func ExampleHashToInt() {
+
+	bloomfilter.HashToInt("pippo4$wrwew")
+	bloomfilter.HashToInt("pippo$wrwew")
+
+	// Output:
+	//
 }
